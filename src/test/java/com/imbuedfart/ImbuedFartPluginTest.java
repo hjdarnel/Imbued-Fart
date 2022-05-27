ab411f55483f12a58954e2e15ddb4cc6cebfa86a
himonn <s.peppitt@yahoo.co.uk>
2022-05-27 13:36:30 +0100

    initial commit

--- src/test/java/com/imbuedfart/ImbuedFartPluginTest.java
+++ src/test/java/com/imbuedfart/ImbuedFartPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.imbuedfart;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ImbuedFartPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ImbuedFartPlugin.class);
 		RuneLite.main(args);
 	}
 }
