1a934de9c21aa5dcec341c72baa310ee2b3ad766
himonn <s.peppitt@yahoo.co.uk>
2022-05-27 13:38:16 +0100

    add plugin files

---
+++ src/main/java/com/imbuedfart/ImbuedFartConfig.java
@@ -0,0 +1,24 @@
+package com.imbuedfart;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
+
+@ConfigGroup("imbuedfart")
+public interface ImbuedFartConfig extends Config {
+
+    @Range(
+            min = 1,
+            max = 10
+    )
+    @ConfigItem(
+            keyName = "volume",
+            name = "Volume",
+            description = "Adjust fart volume",
+            position = 8
+    )
+    default int volume() {
+        return 100;
+    }
+}
---
+++ src/main/java/com/imbuedfart/ImbuedFartPlugin.java
@@ -0,0 +1,90 @@
+package com.imbuedfart;
+
+import com.google.inject.Provides;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.events.CommandExecuted;
+import net.runelite.api.events.SoundEffectPlayed;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+import javax.sound.sampled.*;
+
+@Slf4j
+@PluginDescriptor(
+        name = "Imbued Fart",
+        enabledByDefault = false,
+        description = "ImbuedFart"
+)
+public class ImbuedFartPlugin extends Plugin
+{
+    @Inject
+    private Client client;
+
+    @Inject
+    private ImbuedFartConfig config;
+
+    private Clip clip;
+
+    @Provides
+    ImbuedFartConfig provideConfig(final ConfigManager configManager)
+    {
+        return configManager.getConfig(ImbuedFartConfig.class);
+    }
+
+    @Subscribe
+    public void onCommandExecuted(CommandExecuted event)
+    {
+        if (event.getCommand().equals("fart"))
+        {
+            playFart();
+        }
+    }
+
+    @Subscribe
+    public void onSoundEffectPlayed(SoundEffectPlayed event)
+    {
+        if (event.getSoundId() == 3887)
+        {
+            event.consume();
+            playFart();
+        }
+    }
+
+    public void playFart()
+    {
+        try {
+            if (clip != null)
+            {
+                clip.close();
+            }
+
+            AudioInputStream stream = AudioSystem.getAudioInputStream(ImbuedFartPlugin.class.getResource("fart.wav"));
+
+            if (stream == null)
+            {
+                return;
+            }
+
+            AudioFormat format = stream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            clip = (Clip) AudioSystem.getLine(info);
+
+            clip.open(stream);
+
+            FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            float volumeValue = volume.getMinimum() + ((50 + (config.volume()*5)) * ((volume.getMaximum() - volume.getMinimum()) / 100));
+
+            volume.setValue(volumeValue);
+
+            clip.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
